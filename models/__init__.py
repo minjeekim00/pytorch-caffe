11ce347e388e89f79f00e54b49f186117f0afeaa
minjee <minjeekim00@gmail.com>
2018-11-14 11:45:30 +0900

    added vgg cam model

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from .vggcam import *
---
+++ models/vggcam.py
@@ -0,0 +1,209 @@
+### original code: https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+
+__all__ = [
+    'VGGCAM', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
+    'vgg19_bn', 'vgg19',
+]
+
+
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+
+class VGGCAM(nn.Module):
+
+    def __init__(self, features, num_classes=1000, init_weights=True):
+        super(VGGCAM, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(1024, num_classes),
+        )
+        self.num_classes = num_classes
+        # initialize pretrained classifier weights
+        self._initialize_classifier_weights()
+        if init_weights:
+            self._initialize_weights()
+        
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+    
+    def _initialize_classifier_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight = nn.Parameter(torch.empty(self.num_classes, 1024))
+                m.bias = nn.Parameter(torch.empty(self.num_classes))
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+                
+                
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif v == 'A':
+            layers += [nn.AvgPool2d(kernel_size=(14, 14))]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 1024, 'A'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 1024, 'A'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 1024, 'A'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 1024, 'A'],
+}
+
+def remove_classifier_weight(model_url):
+    pretrained_state = model_zoo.load_url(model_url)
+    pretrained_dict = {k: v for k, v in pretrained_state.items() if k.split('.')[0] != 'classifier'}
+    return pretrained_dict
+    
+
+def vgg11(pretrained=False, **kwargs):
+    """VGGCAM 11-layer model (configuration "A")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['A']), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg11'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def vgg11_bn(pretrained=False, **kwargs):
+    """VGGCAM 11-layer model (configuration "A") with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['A'], batch_norm=True), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg11_bn'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def vgg13(pretrained=False, **kwargs):
+    """VGGCAM 13-layer model (configuration "B")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['B']), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg13'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def vgg13_bn(pretrained=False, **kwargs):
+    """VGGCAM 13-layer model (configuration "B") with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['B'], batch_norm=True), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg13_bn'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def vgg16(pretrained=False, **kwargs):
+    """VGGCAM 16-layer model (configuration "D")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['D']), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg16'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def vgg16_bn(pretrained=False, **kwargs):
+    """VGGCAM 16-layer model (configuration "D") with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['D'], batch_norm=True), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg16_bn'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def vgg19(pretrained=False, **kwargs):
+    """VGGCAM 19-layer model (configuration "E")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['E']), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg19'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+def vgg19_bn(pretrained=False, **kwargs):
+    """VGGCAM 19-layer model (configuration 'E') with batch normalization
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGCAM(make_layers(cfg['E'], batch_norm=True), **kwargs)
+    if pretrained:
+        pretrained_dict = remove_classifier_weight(model_urls['vgg19_bn'])
+        model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
